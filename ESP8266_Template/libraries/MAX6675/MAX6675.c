533a87dcc6eaa56afbfc20701fd93be6fa6e5d46
Nefastor <nefastor@nefastor.com>
2016-08-21 19:14:35 +0200

    Added comments / MIT License

--- ESP8266_Template/libraries/MAX6675/MAX6675.c
+++ ESP8266_Template/libraries/MAX6675/MAX6675.c
@@ -1,22 +1,49 @@
+/*
+ *  Simple bit-banging library for reading temperature samples off a
+ *  MAX6675 and attached K-type thermocouple.
+ *
+ *  Call the "max6675_init" function to setup the ESP8266 I/O pins prior to
+ *  reading the chip.
+ *
+ *  Call the "max6675_read" function to grab the contents of the MAX6675 internal
+ *  register, which is a 16-bit value. See the example code in "user_main.c"
+ *  or read the MAX6675 datasheet for a complete description of the return value.
+ *
+ *  Visit Nefastor Online for more information, including the article connected to
+ *  this code, which describes hardware connections and more :
+ *
+ */
 
+/*
+The MIT License (MIT)
+Copyright (c) 2016 Nefastor Online
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in the
+Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include "MAX6675.h"
 
-//#include "pin_mux_register.h"
 #include "espressif/esp_common.h"	// includes pin_mux_register.h
 #include "gpio.h"
 
-// small delay routine, hopefully written in a way that can't be optimized out by the compiler
-// 5 million equates to roughly 0.5 second.
-void delay (long k)
-{
-	volatile int j = 0;
-	while (j != k) j++;
-}
-
 // Set GPIO pins (D2 as input for SO, D3 and D4 and outputs for CS# and SCK respectively)
 // D2 is GPIO 4, D3 is GPIO 0, D4 is GPIO 2.
-void max6675_init (void)
+inline void max6675_init (void)
 {
 	// First, mux the pins as GPIO
 	// Reminder: the macro and constants are in pin_mux_register.h
@@ -33,31 +60,26 @@ void max6675_init (void)
 	GPIO_OUTPUT_SET (MAX6675_SCK, 0);
 }
 
-uint16 max6675_read (void)
+inline uint16 max6675_read (void)
 {
 	// Start reading a sample by lowering CS#
 	GPIO_OUTPUT_SET (MAX6675_CS, 0);
-	delay (50000);
 
 	// Send 16 SCK pulses and read SO each time
 	int pulse = 16;
 	uint16 retval = 0;
 
-	//for (pulse = 0; pulse < 15; pulse++)
 	while (pulse--)
 	{
 		retval = retval << 1;					// shift the result to clear the LSB for the next bit
 		retval |= GPIO_INPUT_GET (MAX6675_SO);	// read the SO pin
 
 		GPIO_OUTPUT_SET (MAX6675_SCK, 1);		// start a clock pulse
-		delay (5000);
 
 		GPIO_OUTPUT_SET (MAX6675_SCK, 0);		// end a clock pulse
-		delay (5000);
-
 	}
 
-	// After the read is complete, set CS# back up
+	// After the read is complete, set CS# back up (starts a new sampling cycle)
 	GPIO_OUTPUT_SET (MAX6675_CS, 1);
 
 	return retval;
--- ESP8266_Template/libraries/MAX6675/MAX6675.h
+++ ESP8266_Template/libraries/MAX6675/MAX6675.h
@@ -3,7 +3,30 @@
  *
  *  Created on: Aug 20, 2016
  *      Author: Nefastor
- */
+ *
+*/
+
+/*
+The MIT License (MIT)
+Copyright (c) 2016 Nefastor Online
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in the
+Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #ifndef __MAX6675_H__
 #define __MAX6675_H__
@@ -15,8 +38,8 @@
 #define MAX6675_CS		0		// GPIO 0 is NodeMCU's D3
 #define MAX6675_SCK		2		// GPIO 2 is NodeMCU's D4
 
-void max6675_init (void);
+inline void max6675_init (void);		// mux and set I/O pins for MAX6675 operation
 
-uint16 max6675_read (void);
+inline uint16 max6675_read (void);		// read 16 bits from a MAX6675
 
 #endif /* __MAX6675_H__ */
