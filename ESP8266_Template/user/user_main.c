97290bc6949e233341101d74c8c6e195a6262833
Nefastor <nefastor@nefastor.com>
2016-09-07 11:15:21 +0200

    Cosmetic improvements, added useful comments to demo code

--- ESP8266_Template/user/user_main.c
+++ ESP8266_Template/user/user_main.c
@@ -30,45 +30,74 @@
 
 #include "gpio.h"
 
-// Isolate as much MCUnity GUI code as possible
-#include "GUI_code.h"
-
 // GPIO Constants, see pin_mux_register.h (SDK) included through esp_common.h
 #define LED_GPIO 		2		// ESP8266 GPIO pin number, NOT module D pin number
 #define LED_GPIO_MUX	PERIPHS_IO_MUX_GPIO2_U
 #define LED_GPIO_FUNC 	FUNC_GPIO2
 
-// NEW GUI SETUP PARADIGM : use a FreeRTOS task
-// Make it into a "service" that can be triggered by the MCUnity packet parser
-/////////////// WORK IN PROGRESS ////////////////////////////
+// MCUnity setup, phase 1 : declare the global variables MCUnity will let you interact with
+int	exposed_variable = 0x12345678;
+int adc;
+int test = -120;
+int led = 1;
 
-#define SETUP_DELAY	1	// setup delay in FreeRTOS ticks (5 ms @ 160 MHz)
+// MCUnity setup, phase 2 : add in functions that can be triggered from GUI buttons
+void increment_a_variable ()
+{
+	test++;
+}
+
+void toggle_led ()
+{
+	led = 1 - led;
+	GPIO_OUTPUT_SET (LED_GPIO, led);
+}
+
+// MCUnity setup, phase 3 : define GUI tiles appearance flags
+
+// macro parameters : tile location (X and Y), tile size (W and H), two colors, a reserved byte
+// position and dimension are expressed in 1/16th of a display's dimensions.
+// example : a row of 8 tiles of 2 x 2 cells each
+#define	TILE_1	GUI_FLAGS(0,0,2,2,0,0,0)
+#define	TILE_2	GUI_FLAGS(2,0,2,2,0,0,0)
+#define	TILE_3	GUI_FLAGS(4,0,2,2,0,0,0)
+#define	TILE_4	GUI_FLAGS(6,0,2,2,0,0,0)
+#define	TILE_5	GUI_FLAGS(8,0,2,2,0,0,0)
+#define	TILE_6	GUI_FLAGS(10,0,2,2,0,0,0)
+#define	TILE_7	GUI_FLAGS(12,0,2,2,0,0,0)
+#define	TILE_8	GUI_FLAGS(14,0,2,2,0,0,0)
+
+// MCUnity setup, phase 4 : write a task to setup the application's GUI
 
 void task_gui_setup(void *pvParameters)
 {
-	while (1)
+	while (1)		// this loop allows MCUnity to re-setup the GUI if the application requests it
 	{
-		vTaskSuspend (NULL);		// start suspended
+		vTaskSuspend (NULL);		// start suspended, let MCUnity decide when to setup the GUI
 
 		while (unity_not_ready())
 			vTaskDelay (1);	// delay is necessary, let other tasks work while waiting
 		// note : longer delay doesn't help with initial setup glitch
 
+		////////// project-specific GUI setup code starts here ////////////
+
 		// Application (firmware) specific GUI setup operations:
-		unity_setup_int (&exposed_variable,	"A variable", 0, 1000,	GUI1); // GUI_FLAGS(0,0,4,3,0,0,0));
-		unity_setup_int (&adc,				"ADC sample", 0, 1023,			GUI2); // GUI_FLAGS(4,0,4,3,0,0,0));
-		unity_setup_int (&test, "Test Value", -300, 300,			GUI3); // GUI_FLAGS(0,3,4,3,0,0,0));
+		unity_setup_int (&exposed_variable,	"A variable", 0, 1000,	TILE_1); // GUI_FLAGS(0,0,4,3,0,0,0));
+		unity_setup_int (&adc,				"ADC sample", 0, 1023,			TILE_2); // GUI_FLAGS(4,0,4,3,0,0,0));
+		unity_setup_int (&test, "Test Value", -300, 300,			TILE_3); // GUI_FLAGS(0,3,4,3,0,0,0));
 
 		// Setup firmware functions so that they can be called from the Unity application
-		unity_setup_function (increment_counter, "Increment",				GUI4); // GUI_FLAGS(4,3,4,3,0,0,0));
+		unity_setup_function (increment_a_variable, "Increment",				TILE_4); // GUI_FLAGS(4,3,4,3,0,0,0));
 
 		// Setup "int" variable and function call button for the LED
-		unity_setup_int (&led,	"LED State", 0, 1,	GUI5);
-		unity_setup_function (toggle_led, "Toggle LED", GUI6);
+		unity_setup_int (&led,	"LED State", 0, 1,	TILE_5);
+		unity_setup_function (toggle_led, "Toggle LED", TILE_6);
 
 		// Let's add some more !
-//		unity_setup_function (toggle_led, "Toggle LED 2", GUI7);
-//		unity_setup_function (toggle_led, "Toggle LED 3", GUI8);
+//		unity_setup_function (toggle_led, "Toggle LED 2", TILE_7);
+//		unity_setup_function (toggle_led, "Toggle LED 3", TILE_8);
+
+		////////// project-specific GUI setup code ends here ////////////
 
 		// After setting up the GUI, transition to "update" mode
 		unity_setup_rtos_complete ();
@@ -76,6 +105,7 @@ void task_gui_setup(void *pvParameters)
 
 }
 
+// Example of a task that sends updates to the GUI
 
 // sample the ADC periodically
 void task_adc(void *pvParameters)
@@ -84,21 +114,15 @@ void task_adc(void *pvParameters)
 	{
 		adc = system_adc_read();
 
-		// variable has been modified: update the remote GUI
+		// variable has been modified: update the remote GUI :
+		unity_update_int (0, 0);	// update all "int" variables (implicit)
+		// unity_update_int (0, 3);	// update all "int" variables (explicit)
+		// unity_update_int (1, 1);	// update only the "adc" variable (reduced network load)
+		// unity_update_int (1, 2); // update "adc" and the next "int" variable
 
-		// unity_update_int (1, 1);	// restrict update to adc variable
-
-		// unity_update_int (0, 3);	// send all variables (explicit)
-		unity_update_int (0, 0); // send all variables (implicit)
-
-		// unity_update_int (1, 2); // send "adc" and the next variable
-
-
-		vTaskDelay(50);
+		vTaskDelay(50);		// dictates GUI refresh rate : be reasonable
 	}
 }
-
-
 
 
 /******************************************************************************
@@ -124,12 +148,12 @@ void user_init(void)
 	// Multiplex the LED pin as GPIO
 	PIN_FUNC_SELECT(LED_GPIO_MUX, LED_GPIO_FUNC);
 
+	// MCUnity initialization : tell it which task will setup the GUI
 	xTaskHandle setup_tsk;
-    // FreeRTOS task creation : function, name, stack depth, parameter to function, priority, handle
-    // for more details read : http://www.freertos.org/a00125.html
     xTaskCreate(task_gui_setup, "tsk1", 256, NULL, 2, &setup_tsk);
     unity_init_rtos (setup_tsk);
 
-    xTaskCreate(task_adc, "tsk3", 256, NULL, 2, NULL);
+    // Start the firmware's actual tasks, those that matter to you
+    xTaskCreate(task_adc, "tsk2", 256, NULL, 2, NULL);
 }
 
